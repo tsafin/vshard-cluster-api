13554eb29cf8b5c933a188d34a0cf992756fa945
d.voronkov <d.voronkov@corp.mail.ru>
2020-04-15 12:00:13 +0300

    move pub/sub to queue.py

--- api/queue/queue.py
+++ api/queue/queue.py
@@ -1,4 +1,6 @@
-
+"""
+ Queue
+"""
 # create or get queue configuration
 queue, err = vshard.queue(
     channel="system messages"
@@ -32,3 +34,24 @@ data, err = queue.peek(lock=true)
 
 # remove - remove locked message
 data, err = queue.remove(data)
+
+"""
+ Pub/Sub
+"""
+# publish message to a existing or new channel
+pub, err = vshard.publish(
+    channel="user messages"
+    opts = {
+        "read_timeout": "10000", # default parameters
+        "write_timeout": "10000",
+        "size": "100000",
+        "ttl": "100000"})
+
+def handler(message):
+    print(message)
+# subscribe to a channel, specify handler for incoming messages
+sub, err = vshard.subscribe(channel="user messages", handler)
+
+# unsubscribe from a channel
+err = vshard.unsubscribe(channel="user messages")
+
